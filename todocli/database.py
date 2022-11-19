5bf1ab638b3b04f4fcc3d3f3e7de01d4d67f0e05
Thomas Boulier <boulier.thomas@gmail.com>
2022-11-19 18:30:52 +0100

    Communicate with the database

--- todocli/database.py
+++ todocli/database.py
@@ -1,8 +1,11 @@
 """This module provides the To-Do database functionality."""
 
 import configparser
+import json
 from pathlib import Path
-from todocli import DB_WRITE_ERROR, SUCCESS
+from typing import Any, Dict, List, NamedTuple
+
+from todocli import DB_READ_ERROR, DB_WRITE_ERROR, JSON_ERROR, SUCCESS
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath(
     "." + Path.home().stem + "_todo.json"
@@ -24,3 +27,31 @@ def init_database(db_path: Path) -> int:
         return SUCCESS
     except OSError:
         return DB_WRITE_ERROR
+
+
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_todos(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError: # Catch Wrong JSON format
+                    return DBResponse([], JSON_ERROR)
+        except OSError: # Catch file IO problems
+            return DBResponse([], DB_READ_ERROR)
+
+    def write_todos(self, todo_list: List[Dict[str,Any]]) -> DBResponse:
+        try:
+            with self._db_path.open(("w")) as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError: # Catch file IO problems
+            return DBResponse(todo_list, DB_WRITE_ERROR)
