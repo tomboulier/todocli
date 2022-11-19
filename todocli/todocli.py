95c3942a30eb6bbdb01084691012f7f644c20228
Thomas Boulier <boulier.thomas@gmail.com>
2022-11-19 18:33:23 +0100

    Write the Controller Class, Todoer

--- todocli/todocli.py
+++ todocli/todocli.py
@@ -1,8 +1,16 @@
 """This module provides the To-Do model-controller."""
 
+from pathlib import Path
 from typing import Any, Dict, NamedTuple
+
+from todocli.database import DatabaseHandler
 
 
 class CurrentTodo(NamedTuple):
     todo: Dict[str, Any]
     error: int
+
+
+class Todoer:
+    def __init__(self, db_path: Path) -> None:
+        self._db_handler = DatabaseHandler(db_path)
